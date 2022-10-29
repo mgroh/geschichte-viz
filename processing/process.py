f23e01f18f58885f0d19569290d761c58c539728
Michael Groh <michael@mgroh.net>
2022-10-29 23:23:36 +0200

    Add references from file to episodes dicts

--- processing/process.py
+++ processing/process.py
@@ -3,6 +3,7 @@ import xml.etree.ElementTree as ET
 from dateutil.parser import *
 from typing import TypedDict, List
 
+import tomllib
 
 RSS_URL = 'https://www.geschichte.fm/feed/mp3/'
 REFERENCES_FILE = '../data/references.toml'
@@ -40,11 +41,25 @@ def parse_rss_to_dict(rss):
     return [rss_item_to_dict(i) for i in items]
 
 
+def load_references_toml():
+    with open(REFERENCES_FILE, 'rb') as f:
+        return tomllib.load(f)
+
+
+def enrich_episode_with_references(episode, all_references):
+    numeric_id = episode['id'].replace('GAG', '')
+    referenced_episode_ids = ['GAG' + str(x) for x in all_references[numeric_id]]
+    episode['referenced_episodes'] = referenced_episode_ids
+    return episode
+
+
 def main():
     rss = load_rss()
     all_episodes = parse_rss_to_dict(rss)
     gag_episodes = [x for x in all_episodes if x['id'].startswith('GAG')]
-    print(gag_episodes)
+    references = load_references_toml()
+    gag_episodes_with_references = [enrich_episode_with_references(e, references) for e in gag_episodes]
+    print(gag_episodes_with_references)
 
 
 if __name__ == '__main__':
